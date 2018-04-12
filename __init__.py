d91888d70215f15907d0c8f7e57dfd1340be1f06
Matin Hormati <mthormati@gmail.com>
2018-04-11 23:51:01 -0400

    Create routes for login and register

--- __init__.py
+++ __init__.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login', methods=['POST'])
+def login():
+    return 'login'
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return 'register'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
