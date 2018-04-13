7734382d3e0c7bca06035ce0a5c977bc384fc4fe
Matin Hormati <mthormati@gmail.com>
2018-04-13 19:00:51 -0400

    Check if user is valid during log in

--- __init__.py
+++ __init__.py
@@ -5,18 +5,40 @@ import bcrypt
 
 app = Flask(__name__)
 
+app.config['MONGO_DBNAME'] = database_name
+app.config['MONGO_URI'] = database
+
+mongo = PyMongo(app)
+
 @app.route('/')
 def index():
-    errorMessage = ''
-    return render_template('index.html', errorMessage=errorMessage)
+    if 'username' in session:
+        #Change return to render home page
+        return 'Redirect to home page'
+    return render_template('index.html')
+
 
 @app.route('/login', methods=['POST'])
 def login():
-    return 'login'
+    errorMessage = ''
+    #Get user information from database
+    users = mongo.db.users
+    loginUser = users.find_one({'email': request.form['email']})
+    #Check if user exists
+    if loginUser:
+        if bcrypt.hashpw(request.form['password'].encode('utf-8'), loginUser['password']) == loginUser['password']:
+            session['username'] = request.form['email']
+            return redirect(url_for('index'))
+    errorMessage = 'Incorrect username or password'
+    return render_template('index.html', errorMessage=errorMessage)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    #POST register user
+    #GET render register.html
+    errorMessage = ''
     return render_template('register.html')
 
 if __name__ == '__main__':
+    app.secret_key = key
     app.run(debug=True)
