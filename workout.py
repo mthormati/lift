f35acd696150e69c7372044e676564e0c169ea4a
Annie <anniex325@gmail.com>
2018-04-23 05:14:34 -0400

    add

--- workout.py
+++ workout.py
@@ -1,20 +1,4 @@
 class Workout(object):
-<<<<<<< HEAD
-	_id = ""
-	title = ""
-	exercises = []
-	tags = []
-
-
-def make_workout(_id, title, exercises, tags):
-	workout = Workout()
-	workout._id = _id
-	workout.title = title
-	workout.exercises = exercises
-	workout.tags = tags
-
-	return workout
-=======
     title = ""
     exercises = []
     tags = []
@@ -31,4 +15,3 @@ def make_workout(title, owner, exercises, tags, _id):
     workout._id = _id
 
     return workout
->>>>>>> 5b7d9ffcec465ad325b9051f1c43f5b70a32ebd0
