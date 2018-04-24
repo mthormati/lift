e4ad908bdc7b8982df629d15df706e37eee1901a
Matin Hormati <mthormati@gmail.com>
2018-04-24 01:00:02 -0400

    Display profile image on user-card

--- workout.py
+++ workout.py
@@ -4,14 +4,16 @@ class Workout(object):
     tags = []
     _id = ""
     owner = ""
+    image_data = None
 
 
-def make_workout(title, owner, exercises, tags, _id):
+def make_workout(title, owner, exercises, tags, _id, image_data):
     workout = Workout()
     workout.title = title
     workout.exercises = exercises
     workout.tags = tags
     workout.owner = owner
     workout._id = _id
+    workout.image_data = image_data
 
     return workout
